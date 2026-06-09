b61bc50b50dccc568fed25ac687b81da187c6f5f
Islomov-Doniyorbek <islomovdoniyorbey@gmail.com>
2026-06-09 11:24:42 +0500

    Angular material card

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -2,10 +2,14 @@ import { Component, inject, signal } from '@angular/core';
 import { Prd } from '../models/prd';
 import { PrdService } from '../services/prd.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [MatCardModule, MatGridListModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
